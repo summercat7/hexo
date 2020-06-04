f0118a7e3d2ff7d76ce7b2a98e37200755a2aede
wqiuping <wqiuping@linewell.com>
2020-06-04 10:42:26 +0800

    1.0

--- blog.bat
+++ blog.bat
@@ -1,4 +1,4 @@
 :: @echo off
 :: e:
 :: cd E:\Code\nodejs\hexo
-hexo clean&& git add -f --all && git commit -m "1.0" && git push -u origin master &&hexo g&&hexo d
+hexo clean&& git add --all && git commit -m "1.0" && git push -u origin master &&hexo g&&hexo d
